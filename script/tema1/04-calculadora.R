be6e4be08ea93ebe1b6e5045d31c602a41b5b7b3
mlevicoy <mlevicoy@gmail.com>
2021-07-12 16:39:56 -0400

    calc v1

---
+++ script/tema1/04-calculadora.R
@@ -0,0 +1,39 @@
+
+# EJEMPLO DE OPERACIONES MATEMÁTICAS
+
+2 * (3 + 5 / 2)
+
+2 * ((3 + 5) / 2)
+
+2 / 3 + 5
+
+2 %/% 3 + 5
+
+2 %% 3
+
+2^3 * 5
+
+2^-4
+
+2^(-4)
+
+# Dividendo = divisor * cociente + resto
+# r = D - d * q
+# q = D %/% d
+# r = D %% d
+725 %/% 7
+725 %% 7
+725 - 103 * 7
+
+pi
+2 * pi
+3^pi
+pi^2
+
+Inf
+-Inf
+
+NA
+
+NaN
+
