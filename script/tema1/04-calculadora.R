59774c35a8f244117e0ef131e17d47ab0bf47a47
mlevicoy <mlevicoy@gmail.com>
2021-08-03 16:00:51 -0400

    03-08-2021

--- script/tema1/04-calculadora.R
+++ script/tema1/04-calculadora.R
@@ -30,10 +30,52 @@ pi
 3^pi
 pi^2
 
-Inf
--Inf
+pi
 
-NA
+sqrt(9)
+log(exp(1))
+log(1000,10)
+log10(1000)
 
-NaN
+
+factorial(10)
+
+choose(10,2)
+
+choose(5,3)
+
+sin(pi/2)
+
+cos(pi)
+
+tan(0)
+
+cos(0)
+
+print(sqrt(2),10)
+round(sqrt(2),3)
+
+?round
+
+log(x = 32, base = 2)
+
+
+miVariable = 4
+doble = function(x){2*x}
+doble(miVariable)
+
+cuadrado = function(x){x^2}
+cuadrado(miVariable)
+
+xPI = (pi^2)/2
+
+y <- cos(pi/4)
+
+y
+
+sin(pi/4) + cos(pi/4) -> z
+
+
+
+
 
